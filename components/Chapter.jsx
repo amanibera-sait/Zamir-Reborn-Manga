4742a89a74bf97b469efb94642b0ae6ae9325158
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2024-07-09 13:19:21 -0600

    Removed chapter-viewer folder for future rework.

--- components/Chapter.jsx
+++ components/Chapter.jsx
@@ -14,9 +14,9 @@ export default function Chapter({chapter, title, icon, releaseDate, files, likes
                     <p>{likes} Likes</p>
                     <p>{files.length} Pages</p>
                     <p>Published Date: {releaseDate}</p>
-                    <Link href={`/chapter-list/chapter-viewer/chapter-${chapter}`}><button className="rounded  bg-gray-400 font-bold text-gray-900 p-3 hover:bg-gray-500 hover:text-gray-100 active:bg-gray-200">Click to Read</button></Link>   
                 </div>
             </li>
         </>
     )
 }
+                    
