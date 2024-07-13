584d80769647e3cdc731b5898cd28d926d22aa09
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2024-07-12 18:35:21 -0600

    Fixed the banner folder name and removed the contact button.

--- components/layout-components/Header.jsx
+++ components/layout-components/Header.jsx
@@ -13,7 +13,6 @@ export default function Heading(){
                 <div className="flex justify-center items-center text-lg font bold px-10 space-x-7">
                     <Link href="./character-profile" className="rounded bg-gray-400 text-gray-900 p-3 hover:bg-gray-500 hover:text-gray-100 active:bg-gray-200">Character Profiles</Link>
                     <Link href="./chapter-list" className="rounded bg-gray-400 text-gray-900 p-3 hover:bg-gray-500 hover:text-gray-100 active:bg-gray-200">Chapters</Link>
-                    <Link href="./character-profile" className="rounded bg-gray-400 text-gray-900 p-3 hover:bg-gray-500 hover:text-gray-100 active:bg-gray-200">Contacts</Link>
                 </div>
             </div>
         </header>
