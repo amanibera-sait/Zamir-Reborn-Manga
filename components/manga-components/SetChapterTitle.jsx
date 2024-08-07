ad61fb7a23ceef3fef8dc8c8e24f1ecc4b579ec0
Amani Bera <141461158+amanibera-sait@users.noreply.github.com>
2024-08-07 13:27:46 -0600

    Added error handling for SetChapterTitle.jsx.

--- components/manga-components/SetChapterTitle.jsx
+++ components/manga-components/SetChapterTitle.jsx
@@ -8,13 +8,25 @@ const SetChapterTitle = () => {
   const searchParams = useSearchParams();
   const volume = searchParams.get('volume');
   const chapter = searchParams.get('chapter');
-  const title = storyJSON.volumes[volume - 1].chapters[chapter - 1]?.title;
+  
+  function setTitle(){
+    try {
+      const title = storyJSON.volumes[volume - 1].chapters[chapter - 1]?.title;
+      
+      if (chapter && title) {
+        document.title = `Chapter ${chapter} - ${title} | Zamir Reborn`;
+        document.description = storyJSON.synopsis;
+      }
+    }
+    catch (error) {
+      document.title = `Not found`;
+    }
+
+  }
 
   useEffect(() => {
-    if (chapter && title) {
-      document.title = `Chapter ${chapter} - ${title} | Zamir Reborn`;
-    }
-  }, [chapter, title]);
+    setTitle();
+  }, [chapter]);
 
   return null;
 };
